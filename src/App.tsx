9b4abd8e8938bcb4806eae51d5bc3a1154e946c5
robertbrada <bert.blader@gmail.com>
2020-08-09 22:07:02 +0200

    Added Trezor logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,41 @@
 import React from 'react';
 import './App.css';
 import styled from 'styled-components';
+import Logo from './logo.svg';
 
 import SecurityCard from './components/SecurityCard/SecurityCard';
 
 const StepHeadline = styled.div`
     color: #263238;
     font-weight: 500;
-    font-size: 24px;
-    margin-bottom: 2.5em;
+    font-size: 22px;
+    margin-bottom: 1.5em;
 `;
 
 const Wrapper = styled.div`
     display: flex-inline;
     margin: 0 auto;
 
-    @media (max-width: 1090px) {
+    @media (max-width: 1100px) {
         max-width: 600px;
     }
 
-    @media (min-width: 1091px) {
+    @media (min-width: 1101px) {
         max-width: 100%;
     }
+`;
+
+const LogoWrapper = styled.img`
+    position: fixed;
+    width: 110px;
+    left: calc(50% - 55px);
+    top: 25px;
 `;
 
 function App() {
     return (
         <div className="App">
+            <LogoWrapper src={Logo} alt="Trezor Logo" />
             <Wrapper>
                 <StepHeadline>Secure your Trezor 3/4</StepHeadline>
                 <SecurityCard />
